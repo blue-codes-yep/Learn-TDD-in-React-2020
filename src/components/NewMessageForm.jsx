21a92845572a978acaac5516ae3121ca01880fd1
Blue Lindsey <linzblue5@gmail.com>
2020-04-09 11:02:05 -0500

    Completed learntdd in react tutorial

---
+++ src/components/NewMessageForm.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const NewMessageForm = ({ onSend }) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = e => {
+        setInputText(e.target.value);
+    }
+
+    const handleClick = () => {
+        onSend(inputText);
+        setInputText('');
+    }
+    return (
+        <div>
+            <input type="text" data-testid="messageText" value={inputText} onChange={handleChange}></input>
+            <button onClick={handleClick} data-testid="sendButton">Send</button>
+        </div>
+    );
+};
+
+export default NewMessageForm;
